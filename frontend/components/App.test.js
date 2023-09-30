490d2adfc0104ef0069f96e3319d849e66707b2a
Brandon Barksdale <bbark93@gmail.com>
2023-09-30 02:13:30 -0400

    ran test

--- frontend/components/App.test.js
+++ frontend/components/App.test.js
@@ -1,4 +1,16 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+// import userEvent from '@testing-library/user-event';
+
+import AppFunctional from './AppFunctional';
+
 // Write your tests here
 test('sanity', () => {
-  expect(true).toBe(false)
+  expect(true).toBe(true)
 })
+
+
+test('renders without errors', () => {
+  render(<AppFunctional/>);
+});
